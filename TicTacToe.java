82ddd84f2d7987124af871b43d9b5d3b82039837
AV-NASH <avinash21213434@gmail.com>
2020-10-01 10:30:33 +0530

    Adding method to allow player to choose letter

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,11 @@
 package com.cg.tictactoe;
 
+import java.util.Scanner;
+
+/**
+ * @author Avinash
+ *
+ */
 public class TicTacToe {
 
 	public static void main(String[] args) {
@@ -7,6 +13,14 @@ public class TicTacToe {
 		System.out.println("Welcome to the tic tac toe problem");
 
 		char[] tictactoeboard = initializeBoard();
+		char playerletter=chooseLetter();
+		char computerletter;
+		if(playerletter=='X') { 
+			 computerletter='O';
+			}
+		else {
+			 computerletter='X';
+			}
 	}
 
 	/**
@@ -18,7 +32,30 @@ public class TicTacToe {
 		for (int index = 1; index < tictactoeboard.length; index++) {
 			tictactoeboard[index] = ' ';
 		}
-		
+
 		return tictactoeboard;
+	}
+
+	/**
+	 * method to allow player to choose letter
+	 */
+	public static char chooseLetter() {
+		Scanner sc = new Scanner(System.in);
+		char playerletter=' ';
+		System.out.println("Please choose a letter");
+		System.out.println("1. X");
+		System.out.println("2. O");
+		String str = sc.nextLine();
+		char choice=str.charAt(0);
+
+		switch (choice) {
+			case 1: {
+				playerletter = 'X';
+			}
+			case 2: {
+				playerletter = 'O';
+			}
+		}
+		return playerletter;
 	}
 }
